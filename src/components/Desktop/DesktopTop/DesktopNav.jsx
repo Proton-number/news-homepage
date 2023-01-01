17d3746a2d3198c74196e43e511bbd23e611247b
Dacron <favouradebimpe63@gmail.com>
2023-01-01 02:21:45 +0100

    nav adjustment

--- src/components/Desktop/DesktopTop/DesktopNav.jsx
+++ src/components/Desktop/DesktopTop/DesktopNav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Box, Typography, Toolbar} from '@mui/material'
+import { motion } from 'framer-motion'
 
 
 function DesktopNav() {
@@ -8,7 +9,6 @@ function DesktopNav() {
       
       <Toolbar>
 
-        {/* <Box component='img' src={logo} sx={{flexGrow:2}} /> */}
 
     <Typography 
     variant='h1' 
@@ -22,30 +22,45 @@ function DesktopNav() {
 
         <Typography 
         variant='p'   
+        component={motion.h3}
+         whileHover={{textDecoration:'underline', y:-5}}
+        transition={{delay: .3 }}
         sx={{flexGrow:.4}}>
             Home
         </Typography>
 
         <Typography 
         variant='p'   
+         component={motion.h3}
+         whileHover={{textDecoration:'underline', y:-5}}
+        transition={{delay: .3 }}
         sx={{flexGrow:0.4}}>
             New
         </Typography>
 
         <Typography 
-        variant='p'   
+        variant='p'  
+         component={motion.h3}
+         whileHover={{textDecoration:'underline', y:-5}}
+        transition={{delay: .3 }} 
           sx={{flexGrow:.4}}>
             Popular
         </Typography>
 
         <Typography 
-        variant='p'     
+        variant='p' 
+         component={motion.h3}
+         whileHover={{textDecoration:'underline', y:-5}}
+        transition={{delay: .3 }}    
          sx={{flexGrow:.4}}>
             Trending
         </Typography>
 
         <Typography 
-        variant='p'    
+        variant='p' 
+         component={motion.h3}
+         whileHover={{textDecoration:'underline', y:-5}}
+        transition={{delay: .3 }}   
          sx={{flexGrow:.4}}>
         Categories
         </Typography>
